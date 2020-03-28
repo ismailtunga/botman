f3e55b2ba5586776085b7da240843b07a63b1e5c
İsmail TUNGA <i.tunga@hotmail.com>
2020-03-28 18:15:33 +0300

    test

--- my_new_bot/app/Http/Controllers/BotManController.php
+++ my_new_bot/app/Http/Controllers/BotManController.php
@@ -14,8 +14,9 @@ class BotManController extends Controller
     public function handle()
     {
         $botman = app('botman');
-
+        
         $botman->listen();
+  
     }
 
     /**
--- my_new_bot/routes/botman.php
+++ my_new_bot/routes/botman.php
@@ -6,4 +6,12 @@ $botman = resolve('botman');
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
+$botman->hears('Selam', function ($bot) {
+    $bot->reply('Merhaba');
+});
+$botman->hears('Nasılsın', function ($bot) {
+    $bot->reply('İyi seni sormalı');
+});
+
+
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
